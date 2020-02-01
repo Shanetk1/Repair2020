30769bd594d60a927c803a996c06ed3ce64f9d25
EthanMartincourt <54903843+EthanMartincourt@users.noreply.github.com>
2020-01-31 19:57:25 -0500

    Ethan

---
+++ Assets/movementScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(Vector2.left);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(Vector2.left);
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(Vector2.right);
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(Vector2.up);
+		}
+	}
+}

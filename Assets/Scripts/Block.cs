6c3364738343338f762c1919a4364ef5e7afe5c4
Shanetk1 <57373030+Shanetk1@users.noreply.github.com>
2020-01-31 23:30:41 -0500

    BlocksUpdate

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum ColourType
+    {
+        Blue,
+        Red
+
+    }
+}
---
+++ Assets/Scripts/GeneralBlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralBlock : Block
+{
+    private SpriteRenderer Sprite;
+    public ColourType color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprite = GetComponent<SpriteRenderer>();
+        if (color == ColourType.Red)
+        {
+            Sprite.color = Color.red;
+        }
+        else if (color == ColourType.Blue)
+        {
+            Sprite.color = Color.blue;
+        }
+    }
+
+}

4aef63f03ee90e5ba69a4295e1dab44c2971472d
Shanetk1 <57373030+Shanetk1@users.noreply.github.com>
2020-02-02 10:49:56 -0500

    Version 0.4.8
    -Added Mainly Animations to most Game Objects; Power-Ups, and Props

--- Assets/PowerUp.cs
+++ Assets/PowerUp.cs
@@ -6,17 +6,5 @@ public class PowerUp : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    
 
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
